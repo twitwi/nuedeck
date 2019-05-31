dd9b7bcc2592a28c166ccc11b628f54bdd515bed
Rémi Emonet <remi.emonet@reverse--com.heeere>
2019-05-31 21:30:03 +0200

    Simplify StatusBar

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="slide-number">
-    {{ $parent.vars.venue }} |
-    {{ $parent.vars.author }} |
-    {{ $parent.vars.date }} |
-    {{ $parent.renderSlide + 1 }} / {{ $parent.$parent.slideCount }}
+    {{ p.vars.venue }} |
+    {{ p.vars.author }} |
+    {{ p.vars.date }} |
+    {{ $parent.renderSlide + 1 }} / {{ p.slideCount }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'StatusBar',
+  computed: {
+    p () {
+      return this.$parent.$parent
+    }
+  },
   props: {
   }
 }
