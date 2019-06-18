1aef6a98e941ec26d8c5c06c397b05b345e88589
Rémi Emonet <remi.emonet@reverse--com.heeere>
2019-06-18 23:23:56 +0200

    Cleanup warnings

--- src/plugins/markdown-extra.js
+++ src/plugins/markdown-extra.js
@@ -1,18 +1,12 @@
 
 import { startsWithIgnoreCase, RESTRIM, indexOfIgnoreCase } from './tools'
 
-let tools = {
-  L () { console.log(...arguments) }
-}
-
 export default () => ({
   name: 'MarkdownExtra',
   //
   /*async*/ enrichGeneratedSlides({type, body: w, headerLines}) {
 
     if (type !== 'md') return
-
-    let may = (f) => f ? f : ()=>{}
 
     Array.from(w.children).forEach(s => {
 
@@ -23,10 +17,10 @@ export default () => ({
       if (s.firstChild.tagName && s.firstChild.tagName.match(/^h[12]$/i)) {
         if (startsWithIgnoreCase(s.firstChild.textContent, '@COPY:')) {
           var main = RESTRIM().split(/:/);
-          var baseSelector = main[0];
-          var animPart = main.slice(1).join(':');
-          var hasAnim = ! animPart.match(/^\s*$/);
-          var base = null;
+          //var baseSelector = main[0];
+          //var animPart = main.slice(1).join(':');
+          //var hasAnim = ! animPart.match(/^\s*$/);
+          //var base = null;
           s.outerHTML = `<div data-special copy='${main}'>WILL BE REPLACED BY ${main}</div>`
         }
       }
